5d6a32041a08ea6d96325b8677dc084949724632
juliasankina1990-debug <juliasankina1990@gmail.com>
2026-04-21 09:28:22 +0300

    Add BugPro project with Stateless package and stub classes

---
+++ BugPro/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using Stateless;
+
+namespace BugPro
+{
+    public class Bug
+    {
+        // заглушка
+        public Bug() { }
+        public void Analyze() { }
+        public void Defer() { }
+        public void WontFix() { }
+        public void SeparateSolution() { }
+        public void Duplicate() { }
+        public void OtherProduct() { }
+        public void NeedInfo() { }
+        public void CannotReproduce() { }
+        public void StartFix() { }
+        public void Verify() { }
+        public void Yes() { }
+        public void No() { }
+        public void Reopen() { }
+        public string GetState() => "NewDefect";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bug workflow simulator (stub)");
+            var bug = new Bug();
+            Console.WriteLine($"Current state: {bug.GetState()}");
+        }
+    }
+}
